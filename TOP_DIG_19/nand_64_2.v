ef2c8713b17c963c3c258b1f45e20d6b51b2e771
yuma-isobe <m1m18302@oit.ac.jp>
2019-07-02 16:16:25 +0900

    Add new file

--- TOP_DIG_19/nand_64_2.v
+++
@@ -1,7 +0,0 @@
-module nand_64_2 (qout,qout_b, nand_out );
-  input [31:0] qout;
-   input [31:0] qout_b;
-   output [63:0] nand_out;
-   assign nand_out = ~({qout[31:0], qout_b[31:0]} & {qout_b[30:0], qout[31:0],qout_b[31]});
-   
-endmodule
